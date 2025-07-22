0da5d1a387d51704d0f40a472f58e62f70aca6cc
jakubkastner <jakvb.kastner@gmail.com>
2025-07-22 21:40:58 +0200

    cleanup

--- MusicReleases/Store/FilterStore/SpotifyFilterState.cs
+++ MusicReleases/Store/FilterStore/SpotifyFilterState.cs
@@ -1,53 +1,20 @@
 ﻿using Fluxor;
 using JakubKastner.MusicReleases.Objects;
-using JakubKastner.SpotifyApi.Objects;
 
 namespace JakubKastner.MusicReleases.Store.FilterStore;
 
 [FeatureState]
 public class SpotifyFilterState
 {
-	public ISet<SpotifyRelease>? AllReleases { get; }
-	public ISet<SpotifyRelease>? FilteredReleases { get; }
-	public ISet<SpotifyArtist>? AllArtists { get; }
-	public ISet<SpotifyArtist>? FilteredArtists { get; }
-	public Dictionary<int, SortedSet<int>>? FilteredYearMonth { get; }
 	public SpotifyFilter Filter { get; }
 
 	public SpotifyFilterState()
 	{
-		AllReleases = null;
-		FilteredReleases = null;
-		AllArtists = null;
-		FilteredArtists = null;
 		Filter = new();
-	}
-
-	public SpotifyFilterState(ISet<SpotifyRelease> allReleases, ISet<SpotifyRelease> filteredReleases, ISet<SpotifyArtist>? allArtists, ISet<SpotifyArtist>? filteredArtists, Dictionary<int, SortedSet<int>>? filteredYearMonth, SpotifyFilter filter)
-	{
-		AllReleases = allReleases;
-		FilteredReleases = filteredReleases;
-		AllArtists = allArtists;
-		FilteredArtists = filteredArtists;
-		FilteredYearMonth = filteredYearMonth;
-		Filter = filter;
-	}
-
-	public SpotifyFilterState(ISet<SpotifyArtist> allArtists, SpotifyFilter filter)
-	{
-		AllReleases = null;
-		FilteredReleases = null;
-		AllArtists = allArtists;
-		FilteredArtists = allArtists;
-		Filter = filter;
 	}
 
 	public SpotifyFilterState(SpotifyFilter filter)
 	{
-		AllReleases = null;
-		FilteredReleases = null;
-		AllArtists = null;
-		FilteredArtists = null;
 		Filter = filter;
 	}
 }
