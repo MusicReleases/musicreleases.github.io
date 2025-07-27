1b2dca077123a34a58ecd1007a6207c0da434c7e
jakubkastner <jakvb.kastner@gmail.com>
2025-07-27 20:16:12 +0200

    fluxor removed - spotify artist

--- MusicReleases/Store/ApiStore/SpotifyStore/SpotifyArtistStore/SpotifyArtistState.cs
+++
@@ -1,9 +0,0 @@
-using JakubKastner.SpotifyApi.Objects;
-using JakubKastner.SpotifyApi.Objects.Base;
-
-namespace JakubKastner.MusicReleases.Store.ApiStore.SpotifyStore.SpotifyArtistStore;
-
-public record SpotifyArtistState : SpotifyObjectState<SpotifyArtist, SpotifyUserListUpdateMain>
-{
-	public ISet<SpotifyArtist> NewArtists { get; init; } = new HashSet<SpotifyArtist>();
-}
