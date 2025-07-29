f7dc5ba7c359e5305381f76235e2dfbdfe013889
jakubkastner <jakvb.kastner@gmail.com>
2025-07-29 23:02:10 +0200

    added missing file

---
+++ MusicReleases/Web/Components/LoggedIn/Menus/Releases/MenuReleases.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace JakubKastner.MusicReleases.Web.Components.LoggedIn.Menus.Releases;
+
+public partial class MenuReleases
+{
+	[Parameter]
+	public string? Class { get; set; }
+
+}
