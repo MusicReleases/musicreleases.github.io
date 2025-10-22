d9f40650f472f74125b9f79297fa0a7cbb29df89
jakubkastner <jakvb.kastner@gmail.com>
2025-10-22 12:48:53 +0200

    testing

--- MusicReleases/Web/Components/LoggedIn/Menus/Playlists/MenuPlaylists.razor.cs
+++ MusicReleases/Web/Components/LoggedIn/Menus/Playlists/MenuPlaylists.razor.cs
@@ -1,13 +1,10 @@
-﻿using JakubKastner.SpotifyApi.Objects;
-using JakubKastner.SpotifyApi.Objects.Base;
-
-namespace JakubKastner.MusicReleases.Web.Components.LoggedIn.Menus.Playlists;
+﻿namespace JakubKastner.MusicReleases.Web.Components.LoggedIn.Menus.Playlists;
 
 public partial class MenuPlaylists
 {
-	private SpotifyUserList<SpotifyPlaylist, SpotifyUserListUpdatePlaylists>? _playlists => StateSpotifyPlaylist.Value.List;
+	/*private SpotifyUserList<SpotifyPlaylist, SpotifyUserListUpdatePlaylists>? _playlists => StateSpotifyPlaylist.Value.List;
 	private bool Error => StateSpotifyPlaylist.Value.Error;
-	private bool Loading => StateSpotifyPlaylist.Value.LoadingAny();
+	private bool Loading => StateSpotifyPlaylist.Value.LoadingAny();*/
 
 	protected override void OnInitialized()
 	{
