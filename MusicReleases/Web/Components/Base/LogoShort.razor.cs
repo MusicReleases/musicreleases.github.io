b5377b04569870cd8318a6c668a33d73b8d31233
jakubkastner <jakvb.kastner@gmail.com>
2025-11-28 17:07:53 +0100

    home page, scss

---
+++ MusicReleases/Web/Components/Base/LogoShort.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace JakubKastner.MusicReleases.Web.Components.Base;
+
+public partial class LogoShort
+{
+	[Parameter]
+	public bool DisplayMusic { get; set; } = false;
+
+}
