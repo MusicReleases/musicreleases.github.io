abd44d21adcde34056a3e227307cfbd39db89e92
jakubkastner <jakvb.kastner@gmail.com>
2026-02-14 23:32:01 +0100

    fa icons replaced with lucide

--- MusicReleases/Web/Components/LoggedIn/Footers/FooterMobileMenu.razor.cs
+++ MusicReleases/Web/Components/LoggedIn/Footers/FooterMobileMenu.razor.cs
@@ -1,10 +1,16 @@
-﻿namespace JakubKastner.MusicReleases.Web.Components.LoggedIn.Footers;
+﻿using JakubKastner.MusicReleases.Enums;
+
+namespace JakubKastner.MusicReleases.Web.Components.LoggedIn.Footers;
 
 public partial class FooterMobileMenu
 {
 	private string MenuTitle => _displayMenu ? "Hide menu" : "Show menu";
 
+	private LucideIcon Icon => _displayMenu ? LucideIcon.X : LucideIcon.Menu;
+
+
 	private bool _displayMenu = false;
+
 
 	private void ToggleMenu()
 	{
