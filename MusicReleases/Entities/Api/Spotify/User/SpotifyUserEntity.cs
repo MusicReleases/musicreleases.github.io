c93c619c43749ff23e559c7d2ed614570ebd1917
jakubkastner <jakvb.kastner@gmail.com>
2026-03-03 23:02:58 +0100

    new spotify api endpoints

--- MusicReleases/Entities/Api/Spotify/User/SpotifyUserEntity.cs
+++ MusicReleases/Entities/Api/Spotify/User/SpotifyUserEntity.cs
@@ -6,7 +6,6 @@ namespace JakubKastner.MusicReleases.Entities.Api.Spotify.User;
 
 public class SpotifyUserEntity : SpotifyIdNameEntity
 {
-	public required string Country { get; init; }
 	public string? ProfilePictureUrl { get; init; }
 	public required string RefreshToken { get; init; }
 
@@ -17,7 +16,6 @@ public class SpotifyUserEntity : SpotifyIdNameEntity
 	{
 		Id = spotifyUserInfo.Id;
 		Name = spotifyUserInfo.Name;
-		Country = spotifyUserInfo.Country;
 		ProfilePictureUrl = spotifyUserInfo.ProfilePictureUrl;
 		RefreshToken = refreshToken;
 	}
